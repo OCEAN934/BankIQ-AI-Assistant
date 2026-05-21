2e629c34b839947007c1a6d8a6c903dc69c4ea74
Varun Wadhwa <varunwadhwa.10464@gmail.com>
2026-05-21 22:21:36 +0530

    Add package init files for deployment

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,8 +8,14 @@ class Settings:
     VERSION = "1.0.0"
 
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    MODEL_NAME = os.getenv("MODEL_NAME")
-    EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+    MODEL_NAME = os.getenv(
+    "MODEL_NAME",
+    "llama-3.3-70b-versatile"
+)
+    EMBEDDING_MODEL = os.getenv(
+    "EMBEDDING_MODEL",
+    "sentence-transformers/all-MiniLM-L6-v2"
+)
 
     RAW_DATA_PATH = "app/data/raw"
     VECTOR_STORE_PATH = "app/data/vector_store"
